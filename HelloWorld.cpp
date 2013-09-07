736a06fe22be7e3ee6c714b6e8c3665943719f20
wwwhhhyyy <why3713717@163.com>
2013-09-07 12:51:17 +0800

    HomeWork
    HomeWork

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc,char **argv)
+{
+	cout<<"Hello World"<<endl<<"I'm 王宏宇"<<endl;
+	return 0;
+}
+
+/*https://github.com/luckymark/CppExamples */
+/*注册github*/
